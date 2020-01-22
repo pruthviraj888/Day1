115e3ab0ba3ec418e519adb9ce9b2954ebc60d1a
MacStudent <macstudent@128-IMAC36.local>
2020-01-21 20:51:12 -0500

    [PRC] Initial commit

---
+++ src/main/java/com/lambton/FirstExample.java
@@ -0,0 +1,15 @@
+package com.lambton;
+
+import java.util.Arrays;
+
+public class FirstExample
+{
+    public static void main(String[] args) {
+        System.out.println("welcome to JAVA programming");
+        for (int i = 0; i <= 10; i++) {
+            System.out.println("Pruthviraj");
+
+        }
+    }
+
+}
